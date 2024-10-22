44051f849ddcb4ce50b9ba085119ccca60511df5
Matt Lese <matthewjlese@gmail.com>
2024-10-22 14:38:06 -0700

    adds option to copy content to clipboard

--- cmd/content.go
+++ cmd/content.go
@@ -2,12 +2,15 @@ package cmd
 
 import (
     "fmt"
-    "cue/internal"
+		"os"
+    "github.com/atotto/clipboard"
     "github.com/spf13/cobra"
+    "cue/internal"
 )
 
 func init() {
     contentCmd.Flags().BoolP("strip-tags", "s", false, "Strip XML tags from the output")
+    contentCmd.Flags().BoolP("copy", "c", false, "Copy the output to clipboard instead of printing")
     rootCmd.AddCommand(contentCmd)
 }
 
@@ -15,10 +18,12 @@ var contentCmd = &cobra.Command{
     Use:   "content [prompt name]",
     Short: "Get the content of a prompt",
     Long: `Get the content of a prompt. By default, shows content with internal XML tags.
-Use -s or --strip-tags to remove all XML tags from the output.`,
+Use -s or --strip-tags to remove all XML tags from the output.
+Use -c or --copy to copy the content to your clipboard instead of printing to stdout.`,
     Args: cobra.ExactArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
 			stripTags, _ := cmd.Flags().GetBool("strip-tags")
+			copyToClipboard, _ := cmd.Flags().GetBool("copy")
 			promptName := args[0]
 
 			prompt, err := internal.LoadPrompt(promptName + ".xml")
@@ -26,10 +31,18 @@ Use -s or --strip-tags to remove all XML tags from the output.`,
 					return fmt.Errorf("failed to load prompt '%s': %w", promptName, err)
 			}
 
+			content := prompt.Content
 			if stripTags {
-					fmt.Println(internal.StripXMLTags(prompt.Content))
+					content = internal.StripXMLTags(content)
+			}
+
+			if copyToClipboard {
+					if err := clipboard.WriteAll(content); err != nil {
+							return fmt.Errorf("failed to copy to clipboard: %w", err)
+					}
+					fmt.Fprintln(os.Stderr, "Content copied to clipboard")
 			} else {
-					fmt.Println(prompt.Content)
+					fmt.Println(content)
 			}
 
 			return nil
